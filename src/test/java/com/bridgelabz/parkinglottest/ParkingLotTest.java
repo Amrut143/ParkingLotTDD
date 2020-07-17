8c8cd88376a33d3103b2b985d7955f7c2b4c1ea5
Amrut <amrutpanda7789@gmail.com>
2020-07-17 19:40:38 +0530

    ADDED: Test to check the driver able to park the car or not, if parked return true

--- src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
@@ -1,4 +1,16 @@
 package com.bridgelabz.parkinglottest;
 
+import com.bridgelabz.parkinglot.ParkingLot;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest {
+
+    @Test
+    public void givenAVehicle_WhenParkedInParkingLot_ShouldReturnTrue() {
+        ParkingLot parkingLot = new ParkingLot();
+        Object vehicle = new Object();
+        boolean isParked = parkingLot.parkVehicle(vehicle);
+        Assert.assertTrue(isParked);
+    }
 }
