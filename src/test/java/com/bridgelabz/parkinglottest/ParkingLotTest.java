719953a164524fd29519d26f9cb83e52f68bf43c
Amrut <amrutpanda7789@gmail.com>
2020-07-17 21:20:39 +0530

    ADDED: Test to check car is unparked or not, if unparked return true

--- src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
@@ -2,15 +2,30 @@ package com.bridgelabz.parkinglottest;
 
 import com.bridgelabz.parkinglot.ParkingLot;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
 
+    ParkingLot parkingLot;
+    Object vehicle;
+
+    @Before
+    public void setUp() {
+        parkingLot = new ParkingLot();
+        vehicle = new Object();
+    }
+
     @Test
     public void givenAVehicle_WhenParkedInParkingLot_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
-        Object vehicle = new Object();
         boolean isParked = parkingLot.parkVehicle(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.parkVehicle(vehicle);
+        boolean isUnParked = parkingLot.unParkVehicle(vehicle);
+        Assert.assertTrue(isUnParked);
     }
 }
