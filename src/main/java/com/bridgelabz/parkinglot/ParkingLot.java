c5ca5e48a8d96808b9f8470279668c9557752c3d
Amrut <amrutpanda7789@gmail.com>
2020-07-17 21:21:32 +0530

    ADDED: Functionality to unparked the vehicle

--- src/main/java/com/bridgelabz/parkinglot/ParkingLot.java
+++ src/main/java/com/bridgelabz/parkinglot/ParkingLot.java
@@ -1,7 +1,28 @@
 package com.bridgelabz.parkinglot;
 
 public class ParkingLot {
+
+    private Object vehicle;
+
+    /**
+     *
+     * @param vehicle
+     * @return
+     */
     public boolean parkVehicle(Object vehicle) {
+        this.vehicle = vehicle;
         return true;
+    }
+
+    /**
+     *
+     * @param vehicle
+     * @return
+     */
+    public boolean unParkVehicle(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            return true;
+        }
+        return false;
     }
 }
